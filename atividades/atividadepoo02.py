798cb6f1198ce9cf5091e1055e28bafb3795cbab
W. Rodrigues <willybensbergwilly@gmail.com>
2026-09-09 11:41:27 -0300

    POO

--- atividades/atividadepoo02.py
+++ atividades/atividadepoo02.py
@@ -1,18 +1,27 @@
-class Produto:
-    def __init__(self, nome, preco):
-            self.nome = nome
-            self.preco = preco
+class Restaurante:
+    def __init__(self, nome, localizacao, tipo_de_comida, quantidade_funcionarios):
+            self.nome_restaurante = nome
+            self.localizacao = localizacao
+            self.tipo_de_comida = tipo_de_comida
+            self.quantidade_funcionarios = quantidade_funcionarios
             self.ativo = False
-            
-produto01 = Produto("detergente, 12.99")
-print(produto01.nome)
-print(produto01.preco)
 
-produto01 = Produto("Detergente", 12.99)
-produto02 = Produto("Quiboa", 17.99)
-produto03 = Produto("Desinfetante", 42.99)
+    def __str__(self):
+        return f"Nome: {self.nome_restaurante}\n - Rua: {self.localizacao}\n Tipo de Comida: {self.tipo_de_comida}\n - Quantidade de Funcionários: {str(self.quantidade_funcionarios)}"
+    
+    def listar_restaurante():
+        for restaurante in Restaurante.restaurantes:
+            print(f"Nome: {restaurante.nome_restaurante} \n|Rua: {restaurante.localizacao} \n|Tipo de Comida: {restaurante.tipo_de_comida} \n|Quantidade de Funcionários: {str(restaurante.quantidade_funcionarios)} \n|Status: {restaurante.status}")
+ 
+la_mafia = Restaurante("La Mafia", "Rua das Flores, 123", "Comida Italiana", 15)
+mada = Restaurante("Mada", "Rua das Palmeiras, 456", "Comida Brasileira", 20)
+steve_pizza = Restaurante("Steve Pizza", "Rua das Laranjeiras, 789", "Comida Japonesa", 10)
 
-print(produto01)
-print(produto02)
-print(produto03)
+Restaurante.listar_restaurante()
+      
+
+
+
+
+
 
---
+++ atividades/atividadepoo03.py
@@ -0,0 +1,7 @@
+# produto01 = Produto("Detergente", "Localização 1", "Comida Italiana", 5)
+# produto02 = Produto("Quiboa", "Localização 2", "Comida Brasileira", 10)
+# produto03 = Produto("Desinfetante", "Localização 3", "Comida Japonesa", 8)
+
+# print(produto01)
+# print(produto02)
+# print(produto03)
